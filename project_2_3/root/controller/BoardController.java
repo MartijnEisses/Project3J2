3ea6fad0435f0b7fb2fa38dcd6c99d49aa346bd8
dylandale97 <dylandale97@hotmail.com>
2021-03-31 12:47:59 +0200

    update create gridboard

--- project_2_3/root/controller/BoardController.java
+++ project_2_3/root/controller/BoardController.java
@@ -3,10 +3,14 @@ package root.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import root.controller.Controller;
 import root.model.Board;
 import root.model.TicTacToe;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,27 +18,25 @@ public class BoardController extends Controller {
 
     @FXML
     GridPane gridBoard;
-    Board board;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        board = new Board(3, 3);
-        createGrid();
+
+        System.out.println(selectedGame);
+        createGridBoard(new Board(3,3));
     }
 
+    public void createGridBoard(Board b){
+        int[] x = b.getBoard()[0];
+        int[] y = b.getBoard()[1];
 
-    public void createGrid(){
-        int[] x = board.getBoard()[0];
-        int[] y = board.getBoard()[1];
+        Pane p = null;
 
         for (int i = 0; i < x.length; i++){
-
             for (int j = 0; j < y.length; j++){
-
-                //        gridBoard.add(new Label("0"), 0, 0);
-//        gridBoard.add(new Label("0"), 0, 1);
-//        gridBoard.add(new Label("0"), 1, 0);
-
+                p = new Pane();
+                p.setMinSize(150, 150);
+                gridBoard.add(p, i, j);
             }
         }
     }
