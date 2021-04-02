ec5eacb0ead94d852522b42da0112d7be6db6948
dylandale97 <dylandale97@hotmail.com>
2021-04-02 22:21:41 +0200

    update

--- project_2_3/root/Main.java
+++ project_2_3/root/Main.java
@@ -34,7 +34,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        // launch(args);
+        launch(args);
         TicTacToe ttt = new TicTacToe();
         ttt.Start();
     }
