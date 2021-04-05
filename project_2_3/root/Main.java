5fa2e3b72afab9a1d7a6533fe90f82dc62e7fd8c
dylandale97 <dylandale97@hotmail.com>
2021-04-05 19:51:35 +0200

    terugknop geïmplementeerd

--- project_2_3/root/Main.java
+++ project_2_3/root/Main.java
@@ -1,13 +1,15 @@
 package root;
 
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import root.model.TicTacToe;
-
+import javax.swing.*;
 import java.io.IOException;
 
 public class Main extends Application {
@@ -30,7 +32,6 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
@@ -46,9 +47,17 @@ public class Main extends Application {
             root = loader.load();
             primaryStage.setScene(new Scene(root, 1280, 720));
             primaryStage.show();
+            primaryStage.getScene().setOnKeyPressed(e -> goToMain(e.getCode()));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void goToMain(KeyCode code) {
+        if(code.equals(KeyCode.ESCAPE)){
+            int dialogButton = 0;
+            int dialogResult = JOptionPane.showConfirmDialog (null, "Would you like to exit this game?","Warning",dialogButton);
+            if(dialogResult == JOptionPane.YES_OPTION){ setScene("view/main.fxml"); }
+        }
+    }
 }
